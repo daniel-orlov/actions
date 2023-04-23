8793b89250387c2eb3c2a48afea0abadf891c9f9
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-23 17:34:18 +0200

    feat: add tests
    Signed-off-by: Daniel Orlov <daniel.orlov@protonmail.com>

---
+++ transport/handler_test.go
@@ -0,0 +1,32 @@
+package transport_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/daniel-orlov/actions/transport"
+)
+
+func TestHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := transport.Handler()
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "Hello Actions"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
